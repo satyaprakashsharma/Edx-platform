6a45b35475b34ef506cb50e5cd187aae98a3e7fe
SathishKumar Eswaran <seswar@microsoft.com>
2018-02-22 02:03:09 -0800

    adding test case

--- lms/djangoapps/grades/api/v1/tests/test_views.py
+++ lms/djangoapps/grades/api/v1/tests/test_views.py
@@ -10,6 +10,7 @@ from django.test.utils import override_settings
 from mock import MagicMock, patch
 from opaque_keys import InvalidKeyError
 from pytz import UTC
+from provider.oauth2.models import AccessToken
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -332,6 +333,56 @@ class CurrentGradeViewTest(GradeViewTestMixin, APITestCase):
             'passed': False
         }]
         self.assertEqual(resp.data, expected_data)  # pylint: disable=no-member
+
+
+@unittest.skipUnless(settings.FEATURES.get("ENABLE_OAUTH2_PROVIDER"), "OAuth2 not enabled")
+class CourseGradeAllUsersViewClientCredentialsTest(mixins.AccessTokenMixin, TestCase):
+    """ Tests validating the client credentials grant behavior. """
+
+    @classmethod
+    def setUpClass(cls):
+        super(CourseGradeAllUsersViewClientCredentialsTest, cls).setUpClass()
+        cls.namespaced_url = 'grades_api:v1:course_grades_all'
+
+    def setUp(self):
+        super(CourseGradeAllUsersViewClientCredentialsTest, self).setUp()
+        self.user = UserFactory()
+
+    def get_url(self):
+        """
+        Helper function to create the url
+        """
+        base_url = reverse(
+            self.namespaced_url,
+            kwargs={
+                'course_id': self.course_key,
+            }
+        )
+
+        return base_url
+
+    def test_access_token(self):
+        """ Verify the client credentials grant can be used to obtain an access token whose default scopes allow access
+        to the user info endpoint.
+        """
+        oauth_client = ClientFactory(user=self.user)
+        data = {
+            'grant_type': 'client_credentials',
+            'client_id': oauth_client.client_id,
+            'client_secret': oauth_client.client_secret
+        }
+        response = self.client.post(reverse('oauth2:access_token'), data)
+        self.assertEqual(response.status_code, 200)
+
+        access_token = json.loads(response.content)['access_token']
+        expected = AccessToken.objects.filter(client=oauth_client, user=self.user).first().token
+        self.assertEqual(access_token, expected)
+
+        headers = {
+            'HTTP_AUTHORIZATION': 'Bearer ' + access_token
+        }
+        response = self.client.get(self.get_url(), **headers)
+        self.assertEqual(response.status_code, 200)
 
 
 @ddt.ddt
